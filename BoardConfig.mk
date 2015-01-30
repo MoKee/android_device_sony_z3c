037aef0284302edb986206cc895c8c57227f0716
martincz <martincz@mokeedev.com>
2015-01-30 19:28:18 +0800

    mokee bringup
    Change-Id: I193bd78a237f7752d43c858ab966db6ceadfb5d6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2013 The MoKee OpenSource Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,10 +23,10 @@ TARGET_OTA_ASSERT_DEVICE := D5803,D5833,z3c
 
 TARGET_SPECIFIC_HEADER_PATH += device/sony/z3c/include
 
-BOARD_HARDWARE_CLASS += device/sony/z3c/cmhw
+BOARD_HARDWARE_CLASS += device/sony/z3c/mkhw
 
 # Kernel properties
-TARGET_KERNEL_CONFIG := cm_shinano_aries_defconfig
+TARGET_KERNEL_CONFIG := mokee_shinano_aries_defconfig
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 25
