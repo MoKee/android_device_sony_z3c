30b61cd93e2b78311b89f7751b204565f9e5c372
Martincz Gao <martincz@mokeedev.com>
2022-07-19 16:05:25 +0800

    z3c: mokee-ify

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,7 @@ DEVICE_PATH := device/sony/z3c
 TARGET_OTA_ASSERT_DEVICE := z3c
 
 # Kernel properties
-TARGET_KERNEL_CONFIG := lineageos_shinano_aries_defconfig
+TARGET_KERNEL_CONFIG := mokee_shinano_aries_defconfig
 
 # Partition information
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2671771648
--- mokee_z3c.mk
+++ mokee_z3c.mk
@@ -20,10 +20,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from z3c device
 $(call inherit-product, device/sony/z3c/z3c.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common MoKee stuff.
+$(call inherit-product, vendor/mokee/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_z3c
+PRODUCT_NAME := mokee_z3c
 PRODUCT_DEVICE := z3c
 PRODUCT_BRAND := Sony
 PRODUCT_MODEL := Xperia Z3 Compact
